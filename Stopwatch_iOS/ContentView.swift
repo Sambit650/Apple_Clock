7fecd62a83d64d8857a74597fca9b53cdedccb3e
Sambit Das <sambit650@gmail.com>
2023-08-30 23:34:05 +0530

    Initial Commit

---
+++ Stopwatch_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stopwatch_iOS
+//
+//  Created by Sambit Das on 30/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stopwatch_iOS/Stopwatch_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stopwatch_iOSApp.swift
+//  Stopwatch_iOS
+//
+//  Created by Sambit Das on 30/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Stopwatch_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
