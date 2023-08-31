9688507c8212674d457426d481283434232ebdf5
Sambit Das <sambit650@gmail.com>
2023-09-01 01:38:07 +0530

    lap functionalty added

--- Stopwatch_iOS/ContentView.swift
+++ Stopwatch_iOS/ContentView.swift
@@ -7,28 +7,43 @@
 
 import SwiftUI
 
+struct LapData {
+  let index: String
+  let value: String
+}
+
 struct ContentView: View {
 
   @State var isRunning: Bool = false
   @State var timerElapsed: TimeInterval = 0
+  @State var laps: [LapData] = []
   private let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
 
   var body: some View {
-    VStack(alignment: .center, spacing: 32) {
-      Image(systemName: "stopwatch")
-        .resizable()
-        .imageScale(.large)
-        .frame(width: 55, height: 58)
-        .foregroundStyle(.tint)
-        Text("\(timeString(time: timerElapsed))")
-          .font(.largeTitle)
-          .fontWeight(.black)
-      HStack(spacing: 24) {
+    VStack {
+      Text("\(timeString(time: timerElapsed))")
+        .font(.system(size: 78))
+        .fontWeight(.light)
+        .frame(maxWidth: .infinity)
+        .padding(.vertical, 80)
+      HStack {
         resetButton
+        Spacer()
         startAndStopButton
       }
+      ScrollView {
+        Divider()
+        ForEach(laps, id: \.index) { lap in
+          HStack {
+            Text(lap.index)
+            Spacer()
+            Text(lap.value)
+          }
+          Divider()
+        }
+      }
     }
-    .padding(.all, 24)
+    .padding(.horizontal, 16)
     .onReceive(timer) { _ in
       if self.isRunning {
         self.timerElapsed += 0.1
@@ -37,47 +52,43 @@ struct ContentView: View {
   }
 
   private var resetButton: some View {
-    Button {
-      resetTimer()
-    } label: {
-      HStack {
-        Image(systemName: "arrow.clockwise")
-          .imageScale(.large)
-          .foregroundStyle(.white)
-        Text("Reset")
-          .foregroundStyle(.white)
-          .fontWeight(.semibold)
-          .font(.title)
-          .frame(maxWidth: .infinity)
-      }
-      .padding()
+    ZStack {
+      Circle()
+        .frame(width: 80, height: 80)
+        .foregroundColor(Color.gray.opacity(0.4))
+
+      Text(isRunning ? "Lap" : "Reset")
+        .fontWeight(.semibold)
+        .font(.headline)
+        .foregroundColor(.white)
     }
-    .background(Color.red)
-    .clipShape(.capsule)
+    .onTapGesture {
+      if isRunning {
+        lapTimer()
+      } else {
+        resetTimer()
+      }
+    }
   }
 
   private var startAndStopButton: some View {
-    Button {
+    ZStack {
+      Circle()
+        .frame(width: 80, height: 80)
+        .foregroundColor(isRunning ? Color.red.opacity(0.3) : Color.green.opacity(0.3))
+
+      Text(isRunning ? "Stop" : "Start")
+        .fontWeight(.semibold)
+        .font(.headline)
+        .foregroundColor(isRunning ? Color.red : Color.green)
+    }
+    .onTapGesture {
       if isRunning {
         stopTimer()
       } else {
         startTimer()
       }
-    } label: {
-      HStack(spacing: 8) {
-        Image(systemName: isRunning ? "pause.fill" : "play.fill")
-          .imageScale(.large)
-          .foregroundStyle(.white)
-        Text(isRunning ? "Stop" : "Start")
-          .foregroundStyle(.white)
-          .fontWeight(.semibold)
-          .font(.title)
-          .frame(maxWidth: .infinity)
-      }
-      .padding()
     }
-    .background(isRunning ? Color.red : Color.green)
-    .clipShape(.capsule)
   }
 
   private func startTimer() {
@@ -91,13 +102,18 @@ struct ContentView: View {
   private func resetTimer() {
     isRunning = false
     timerElapsed = 0
+    laps.removeAll()
+  }
+
+  private func lapTimer() {
+    laps.insert(LapData(index: "Lap \(laps.count+1)", value: timeString(time: timerElapsed)), at: 0)
   }
 
   private func timeString(time: TimeInterval) -> String {
     let minutes = Int(time) / 60
     let seconds = Int(time) % 60
     let miliseconds = Int((time.truncatingRemainder(dividingBy: 1)) * 100)
-    return String(format: "%02d.%02d:%02d", minutes, seconds, miliseconds)
+    return String(format: "%02d:%02d.%02d", minutes, seconds, miliseconds)
   }
 }
 
